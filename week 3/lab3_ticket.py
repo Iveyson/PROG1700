053ad55a9f1eef9d17cc7c8adb81f5094feef4d7
Iveyson <ethan.iveyson@gmail.com>
2025-09-23 15:17:52 -0300

    ws03 edit

--- week 3/lab3_ticket.py
+++ week 3/lab3_ticket.py
@@ -1,7 +1,10 @@
 x = int(input("Insert age: "))
 age = x
+student = bool(input ("Are you a student? type anything if yes if no press enter: "))
 if age < 5:
     print("Your ticket is free")
+elif age < 13 and student == True:
+    print("You get the student discount! $2 please")
 elif age == 5 or age < 13:
     print ("Your ticket is $8")
 elif age ==13 or age <65:
