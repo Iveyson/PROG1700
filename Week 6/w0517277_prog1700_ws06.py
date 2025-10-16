0919dff32f6f2b013e4964d49637d7df31c13bc9
Iveyson <ethan.iveyson@gmail.com>
2025-10-16 15:14:02 -0300

    ws06done

--- Week 6/w0517277_prog1700_ws06.py
+++ Week 6/w0517277_prog1700_ws06.py
@@ -39,6 +39,8 @@ while play == "Y":
     else:
         break
 """
+##ran into issues attempting to install pygame to add sound effects. File Path issues
+"""
 import pygame
 pygame.init()
 pygame.mixer.init()
@@ -59,3 +61,14 @@ while player1_score < 3 and player2_score < 3:
         player2_score += 1
         time.sleep(1)
 print("Winner:", "Player 1" if player1_score > player2_score else "Player 2")
+"""
+
+# Reflection:
+# 1. What bug or mistake caused your first infinite loop today?
+# I did not have any infinite loops today but I did run into some issues importing a few python modules due to a file pathing error that went unresolved
+# 2. How did you fix it?
+# I did not fix it I went into the CLI and reset the file pathing for my userprofile and still couldn't use pip install
+# 3. Which loop activity did you enjoy most, and why?
+# I enjoyed the loop activity with the dice battle simulator since its the one I spent the most time on problem solving to attempt to get audio working
+# 4. How might while loops be useful in a game or app you’d build someday?
+# while loops are useful for any counter based loops or repeating until conditions change. For example an atm machine is a good example.
